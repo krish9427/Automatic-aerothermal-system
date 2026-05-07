d6ef47334861ffd5241c93179108275a9bee0785
PRAJAPATI KRISH <200385108+krish9427@users.noreply.github.com>
2026-05-07 15:33:28 +0530

    Create secrets.h for Wi-Fi and device credentials
    Add a header file for storing Wi-Fi and device credentials.

---
+++ src/secrets.h
@@ -0,0 +1,6 @@
+#pragma once
+// Copy this to secrets.h and fill in your values
+
+#define WIFI_SSID      "YOUR_WIFI_SSID"
+#define WIFI_PASSWORD  "YOUR_WIFI_PASSWORD"
+#define DEVICE_PASS_KEY "YOUR_DEVICE_KEY"
